fd9bd617ec40212bed18e8eb017cd5fa6f2dc9e8
Mervyl Canlas <mervylcanlas@gmail.com>
2021-08-10 12:05:46 +0800

    added task model validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
     belongs_to :category
+    validates :title, presence: true, uniqueness: {scope: :category}
+    validates :description, presence: true, length: { minimum: 16 }
+    validates :deadline, presence: true
 end
